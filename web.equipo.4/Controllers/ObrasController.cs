8fa8f3d0533bb411b28d73f5ab6f9a0b7190bc05
Roberto Bontá <profesorrobertobonta@gmail.com>
2020-05-27 22:34:57 -0300

    Se agrego el Logo y se modifico el CSS

---
+++ web.equipo.4/Controllers/ObrasController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web.equipo._4.Controllers
+{
+    public class ObrasController : Controller
+    {
+        // GET: Obras
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Obras/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Obras/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Obras/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Obras/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Obras/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Obras/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Obras/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
