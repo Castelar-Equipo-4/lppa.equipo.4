87d6c61b5b52ecfd1440b320f9ca75c4a0123bfd
Favio Damian Contreras <FavioDamian.Contreras@Alumnos.uai.edu.ar>
2020-06-15 15:38:28 -0300

    website

---
+++ lppa.equipo.4.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lppa.equipo._4.Website.MvcApplication" Language="C#" %>
---
+++ lppa.equipo.4.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(lppa.equipo._4.Website.Startup))]
+namespace lppa.equipo._4.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
