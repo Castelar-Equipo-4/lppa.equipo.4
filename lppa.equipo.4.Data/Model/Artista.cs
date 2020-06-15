45b081c5497d9b923295fa42cb98f87a499dbdd4
Roberto Bontá <profesorrobertobonta@gmail.com>
2020-06-15 14:35:30 -0300

    Clase Artista

--- lppa.equipo.4.Data/Model/Artista.cs
+++ lppa.equipo.4.Data/Model/Artista.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace lppa.equipo._4.Data.Model
 {
-    class Artista
+   public class Artista : Identity
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LifeSpan { get; set; }
+        //public string Country { get; set; }
+        //public string Description { get; set; }
+
     }
 }
