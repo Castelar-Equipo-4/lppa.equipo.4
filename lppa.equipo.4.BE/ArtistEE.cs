0bba3a66294fe593d2dac172a13ba1c64f4e2dbe
Favio Damian Contreras <FavioDamian.Contreras@Alumnos.uai.edu.ar>
2020-06-17 23:15:19 -0300

    entidad artista

---
+++ lppa.equipo.4.BE/ArtistEE.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lppa.equipo._4.BE
+{
+    public class ArtistEE
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LifeSpan { get; set; }
+        public string Country { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
